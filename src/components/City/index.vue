9e942d260129c6bbf09b81fa50bd2a05896c9bf1
Chen-12138 <2464774230@qq.com>
2020-09-20 21:32:04 +0800

    add setData

--- src/components/City/index.vue
+++ src/components/City/index.vue
@@ -1,15 +1,9 @@
 <template>
     <div class="city_body">
-        <div class="city_list">
+        <!-- <div class="city_list">
             <div class="city_hot">
                 <h2>热门城市</h2>
                 <ul class="clearfix">
-                    <li>上海</li>
-                    <li>北京</li>
-                    <li>上海</li>
-                    <li>北京</li>
-                    <li>上海</li>
-                    <li>北京</li>
                     <li>上海</li>
                     <li>北京</li>
                 </ul>
@@ -24,51 +18,6 @@
                         <li>安阳</li>
                     </ul>
                 </div>
-                <div>
-                    <h2>B</h2>
-                    <ul>
-                        <li>北京</li>
-                        <li>保定</li>
-                        <li>蚌埠</li>
-                        <li>包头</li>
-                    </ul>
-                </div>
-                <div>
-                    <h2>A</h2>
-                    <ul>
-                        <li>阿拉善盟</li>
-                        <li>鞍山</li>
-                        <li>安庆</li>
-                        <li>安阳</li>
-                    </ul>
-                </div>
-                <div>
-                    <h2>B</h2>
-                    <ul>
-                        <li>北京</li>
-                        <li>保定</li>
-                        <li>蚌埠</li>
-                        <li>包头</li>
-                    </ul>
-                </div>
-                <div>
-                    <h2>A</h2>
-                    <ul>
-                        <li>阿拉善盟</li>
-                        <li>鞍山</li>
-                        <li>安庆</li>
-                        <li>安阳</li>
-                    </ul>
-                </div>
-                <div>
-                    <h2>B</h2>
-                    <ul>
-                        <li>北京</li>
-                        <li>保定</li>
-                        <li>蚌埠</li>
-                        <li>包头</li>
-                    </ul>
-                </div>	
             </div>
         </div>
         <div class="city_index">
@@ -79,13 +28,65 @@
                 <li>D</li>
                 <li>E</li>
             </ul>
+        </div> -->
+        <div class="city_list">
+            <div class="city_hot">
+                <h2>热门城市</h2>
+                <ul class="clearfix">
+                    <li v-for="item in hotList" :key="item.id">{{item.nm}}</li>
+                </ul>
+            </div>
+            <div class="city_sort" ref="city_sort">
+                <div v-for="item in cityList" :key="item.index">
+                    <h2>{{item.index}}</h2>
+                    <ul>
+                        <li v-for="itemList in item.list" :key="itemList.id">{{itemList.nm}}</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="city_index">
+            <ul>
+                <li v-for="(item,index) in cityList" :key="item.id" @touchstart="handleToIndex(index)">{{item.index}}</li>
+            </ul>
         </div>
     </div>
 </template>
 
 <script>
 export default {
-    name : 'City'
+    name : 'City',
+    data(){
+        return {
+            cityList : [],
+            hotList : []
+        }
+    },
+    mounted(){
+        this.axios.get('http://localhost:8080/city.json').then((res)=>{
+            // console.log(res)
+            var data = res.data.letterMap
+            for(var k in data){
+                // console.log(k)
+                this.cityList.push({index : k , list : data[k]})
+                // console.log(this.cityList)
+                for(var key in data[k]){
+                    // console.log(data[k][key])
+                    if(data[k][key].rank === 'S' || data[k][key].rank === 'A'){
+                        this.hotList.push(data[k][key])
+                    }
+                }
+            }
+            // console.log(this.hotList)
+
+        })
+    },
+    methods : {
+        handleToIndex(index){
+            var h2 = this.$refs.city_sort.getElementsByTagName('h2')
+            this.$refs.city_sort.parentNode.scrollTop = h2[index].offsetTop
+        }
+    }
 }
 </script>
 
--- src/components/NowPlaying/index.vue
+++ src/components/NowPlaying/index.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="movie_body">
         <ul>
-            <li>
+            <!-- <li>
                 <div class="pic_show"><img src="/images/movie_1.jpg"></div>
                 <div class="info_list">
                     <h2>无名之辈</h2>
@@ -12,86 +12,14 @@
                 <div class="btn_mall">
                     购票
                 </div>
-            </li>
-            <li>
-                <div class="pic_show"><img src="/images/movie_2.jpg"></div>
+            </li> -->
+            <li v-for="item in movieList" :key="item.id">
+                <div class="pic_show"><img :src="item.img | setWH('128.180')"></div>
                 <div class="info_list">
-                    <h2>毒液：致命守护者</h2>
-                    <p>观众评 <span class="grade">9.3</span></p>
-                    <p>主演: 汤姆·哈迪,米歇尔·威廉姆斯,里兹·阿迈德</p>
-                    <p>今天56家影院放映443场</p>
-                </div>
-                <div class="btn_mall">
-                    购票
-                </div>
-            </li>
-            <li>
-                <div class="pic_show"><img src="/images/movie_1.jpg"></div>
-                <div class="info_list">
-                    <h2>无名之辈</h2>
-                    <p>观众评 <span class="grade">9.2</span></p>
-                    <p>主演: 陈建斌,任素汐,潘斌龙</p>
-                    <p>今天55家影院放映607场</p>
-                </div>
-                <div class="btn_mall">
-                    购票
-                </div>
-            </li>
-            <li>
-                <div class="pic_show"><img src="/images/movie_2.jpg"></div>
-                <div class="info_list">
-                    <h2>毒液：致命守护者</h2>
-                    <p>观众评 <span class="grade">9.3</span></p>
-                    <p>主演: 汤姆·哈迪,米歇尔·威廉姆斯,里兹·阿迈德</p>
-                    <p>今天56家影院放映443场</p>
-                </div>
-                <div class="btn_mall">
-                    购票
-                </div>
-            </li>
-            <li>
-                <div class="pic_show"><img src="/images/movie_1.jpg"></div>
-                <div class="info_list">
-                    <h2>无名之辈</h2>
-                    <p>观众评 <span class="grade">9.2</span></p>
-                    <p>主演: 陈建斌,任素汐,潘斌龙</p>
-                    <p>今天55家影院放映607场</p>
-                </div>
-                <div class="btn_mall">
-                    购票
-                </div>
-            </li>
-            <li>
-                <div class="pic_show"><img src="/images/movie_2.jpg"></div>
-                <div class="info_list">
-                    <h2>毒液：致命守护者</h2>
-                    <p>观众评 <span class="grade">9.3</span></p>
-                    <p>主演: 汤姆·哈迪,米歇尔·威廉姆斯,里兹·阿迈德</p>
-                    <p>今天56家影院放映443场</p>
-                </div>
-                <div class="btn_mall">
-                    购票
-                </div>
-            </li>
-            <li>
-                <div class="pic_show"><img src="/images/movie_1.jpg"></div>
-                <div class="info_list">
-                    <h2>无名之辈</h2>
-                    <p>观众评 <span class="grade">9.2</span></p>
-                    <p>主演: 陈建斌,任素汐,潘斌龙</p>
-                    <p>今天55家影院放映607场</p>
-                </div>
-                <div class="btn_mall">
-                    购票
-                </div>
-            </li>
-            <li>
-                <div class="pic_show"><img src="/images/movie_2.jpg"></div>
-                <div class="info_list">
-                    <h2>毒液：致命守护者</h2>
-                    <p>观众评 <span class="grade">9.3</span></p>
-                    <p>主演: 汤姆·哈迪,米歇尔·威廉姆斯,里兹·阿迈德</p>
-                    <p>今天56家影院放映443场</p>
+                    <h2>{{item.nm}} <img v-if="item.version" src="@/assets/maxs.png"></h2>
+                    <p>观众评 <span class="grade">{{item.sc}}</span></p>
+                    <p>主演: {{item.star}}</p>
+                    <p>{{item.showInfo}}</p>
                 </div>
                 <div class="btn_mall">
                     购票
@@ -103,7 +31,20 @@
 
 <script>
 export default {
-    name : 'NowPlaying'
+    name : 'NowPlaying',
+    data(){
+        return {
+            movieList : []
+        }
+    },
+    mounted(){
+        this.axios.get('/ajax/movieOnInfoList?token=&optimus_uuid=876816D0E11511EAAD2009648C480E2D4F65E760198B4C4A96C28AD377AF9033&optimus_risk_level=71&optimus_code=10').then((res)=>{
+            var msg = res.data
+            if(msg){
+                this.movieList = res.data.movieList
+            }
+        })
+    }
 }
 </script>
 
